e0f9f2480eb95c80ffc82da0e7f6c3b48dcc7f7a
Shashank Patel <patel784@uwindsor.ca>
2021-10-20 01:04:13 -0400

    music player link

--- app/src/main/java/com/example/myapplication/music.java
+++ app/src/main/java/com/example/myapplication/music.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class music extends AppCompatActivity {
-
+    private Button InstrumentalButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_categories);
+
+        InstrumentalButton = findViewById(R.id.instrumental);
+        InstrumentalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenPlayer();
+            }
+        });
+
+    }
+
+    public void OpenPlayer() {
+
+        Intent i = new Intent(this,activity_music_player.class);
+        startActivity(i);
     }
 }
